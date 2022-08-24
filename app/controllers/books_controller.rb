958e29ff5a4c1421fa45065403c27efbf33866e6
OONOSSHINPEI <110380501+OONOSSHINPEI@users.noreply.github.com>
2022-08-24 13:21:47 +0000

    [fin4]kadi3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,59 @@
 class BooksController < ApplicationController
   def new
+    @books = Book.new
+  end
+  
+  def create
+    
+    @books = Book.all
+    @new_book = Book.new(book_params)
+    @new_book.user_id = current_user.id
+    if @new_book.save
+      flash[:notice] = "Book was successfully created"
+     redirect_to book_path(@new_book)
+    else
+     render :index
+    end
+    
   end
 
   def index
+    @books = Book.all
+    @new_book = Book.new
+    
   end
 
   def show
+    @book = Book.find(params[:id])
+    @user = @book.user
+    @new_book = Book.new
+  end
+  
+  def edit
+    @book = Book.find(params[:id])
+   
+  end
+  
+  def destroy
+    book = Book.find(params[:id])  
+    book.destroy  
+    redirect_to '/books'  
+    flash[:notice] = "Book was successfully destroy" 
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+  if @book.update(book_params)
+    flash[:notice] = "You have updated book successfully."
+     redirect_to book_path(@book.id)
+  else
+     render :edit
+  end
+
+  end
+ private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body, :image)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+ 
     devise_for :users
     root to: "homes#top"
+    get "about" => "homes#about"
     get "top" => "homes#top"
-    get "homes/about" => "homes#about"
-    resources :books, only: [:new, :index, :show]
+    resources :books
+    resources :users 
+ 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
